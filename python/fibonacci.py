27ca77f44974fc13e12bbb427d3c7932575b7253
interdimensionalpizza <michaellake05@gmail.com>
2023-06-08 12:57:26 -0700

    finished python challenge

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,4 +1,20 @@
 def fibonacci(n):
-  pass
+  a = 0
+  b = 1
+  c = 0
+  for _ in range(0, n-1, 1):
+    c = a + b
+    a = b
+    b = c
+  return c
 
 
+
+print(fibonacci(0))
+print(fibonacci(2))
+print(fibonacci(5))
+print(fibonacci(8))
+print(fibonacci(11))
+print(fibonacci(14))
+print(fibonacci(17))
+print(fibonacci(20))
